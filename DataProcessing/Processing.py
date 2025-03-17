03d0f498ecb0a36221af10c95ed29cfa57aa7daf
zttt04 <1611817463@qq.com>
2025-03-17 21:36:13 +0800

    Update Processing.py

--- DataProcessing/Processing.py
+++ DataProcessing/Processing.py
@@ -2,17 +2,18 @@ import pandas as pd
 import os
 
 # 定义Train文件夹路径
-train_folder = os.path.join('DataProcessing', 'Val')
+train_folder = os.path.join('DataProcessing', 'Train')
 # 获取Train文件夹下所有.parquet文件路径
 train_files = [os.path.join(train_folder, file) for file in os.listdir(train_folder) if file.endswith('.parquet')]
 # 读取并合并数据
 combined_train_df = pd.concat([pd.read_parquet(file) for file in train_files], ignore_index=True)
 # 保存合并后的数据
-combined_train_path = os.path.join(train_folder, 'val.parquet')
+combined_train_path = os.path.join(train_folder, 'train.parquet')
 combined_train_df.to_parquet(combined_train_path, index=False)
-"""combined_train_path = os.path.join('DataProcessing', 'Train', 'combined_train.parquet')
+
+combined_train_path = os.path.join('DataProcessing', 'Train', 'combined_train.parquet')
 combined_train_df = pd.read_parquet(combined_train_path)
-"""
+
 # 提取数据
 extracted_data = []
 for _, row in combined_train_df.iterrows():
@@ -29,23 +30,23 @@ print("提取后的en和de列数据预览：")
 print(extracted_df.head())
 
 # 保存为 .parquet 文件
-train_folder = os.path.join('DataProcessing', 'Val')
+train_folder = os.path.join('DataProcessing', 'Train')
 
 # 保存 en 数据
-en_path = os.path.join(train_folder, 'en_data_val.parquet')
+en_path = os.path.join(train_folder, 'en_data_train.parquet')
 extracted_df[['en']].dropna().to_parquet(en_path, index=False)
 
 # 保存 de 数据
-de_path = os.path.join(train_folder, 'de_data_val.parquet')
+de_path = os.path.join(train_folder, 'de_data_train.parquet')
 extracted_df[['de']].dropna().to_parquet(de_path, index=False)
 
 print(f"en 数据已保存到: {en_path}")
 print(f"de 数据已保存到: {de_path}")
 def get_file_size(file_path):
     return os.path.getsize(file_path)
-train_folder = os.path.join('DataProcessing', 'Val')
-en_path = os.path.join(train_folder, 'en_data_val.parquet')
-de_path = os.path.join(train_folder, 'de_data_val.parquet')
+train_folder = os.path.join('DataProcessing', 'train')
+en_path = os.path.join(train_folder, 'en_data_train.parquet')
+de_path = os.path.join(train_folder, 'de_data_train.parquet')
 if os.path.exists(en_path):
     en_size = get_file_size(en_path)
     en_df = pd.read_parquet(en_path)
