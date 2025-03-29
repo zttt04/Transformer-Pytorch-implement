430c24881b27a8f320b2385d26e8067f15815853
zttt04 <1611817463@qq.com>
2025-03-29 13:47:41 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ tokenizer = FSMTTokenizer.from_pretrained(mname)
 SRC_VOCAB_SIZE = tokenizer.vocab_size
 TGT_VOCAB_SIZE = tokenizer.vocab_size
 class TransformerConfig:
-    def __init__(self, max_len=128, d_model=512, vocab_size=TGT_VOCAB_SIZE, d_ff=2048, dropout=0.1, n_head=8, n_layer=6,device='cuda' if torch.cuda.is_available() else 'cpu',batch_size=64, epochs=10, warmup_steps=4000):
+    def __init__(self, max_len=128, d_model=512, vocab_size=TGT_VOCAB_SIZE, d_ff=2048, dropout=0.1, n_head=8, n_layer=6,device='cuda' if torch.cuda.is_available() else 'cpu',batch_size=64, epochs=2, warmup_steps=4000):
         self.max_len = max_len
         self.d_model = d_model
         self.vocab_size = vocab_size
